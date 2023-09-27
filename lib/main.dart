e9dfe31a2390372decff3643741cc35c528af75d
infitry <leesw504@gmail.com>
2023-09-28 07:59:36 +0900

    fix: show life cycle

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,13 +30,14 @@ class _AppState extends State<App> {
           ),
         ),
       ),
-      home: const Scaffold(
+      home: Scaffold(
         backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? MyLargeTitle() : Text('nothing'),
+              IconButton(onPressed: toggleTitle, icon: Icon(Icons.view_agenda))
             ],
           ),
         ),
@@ -37,13 +46,33 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('Hello!');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("bye!");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build!');
     return Text(
       'My Large TItle',
       style: TextStyle(
