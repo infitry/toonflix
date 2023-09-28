e22a68a23d1c1c3ac3a85d092d81322c49745dac
infitry <leesw504@gmail.com>
2023-09-28 13:00:11 +0900

    fix: api servcice 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screen/home_screen.dart';
+import 'package:toonflix/service/api_service.dart';
 
 void main() {
+  ApiService().getTodayWebtoon();
   runApp(const App());
 }
 
--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,7 +1,16 @@
+import 'package:http/http.dart' as http;
+
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcorders.workers.dev";
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String todayPath = "/today";
 
-  void getTodaysToons() {
+  void getTodayWebtoon() async {
+    final url = Uri.parse('$baseUrl$todayPath');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+      return;
+    }
+    throw Exception("Today Server ERROR");
   }
 }
