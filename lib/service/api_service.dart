f5cfacc8ba1a7817ed921a66d306dddde0da2cb4
infitry <leesw504@gmail.com>
2023-09-28 13:20:47 +0900

    fix: add webtoon model and convert json to model

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/model/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String todayPath = "/today";
 
-  void getTodayWebtoon() async {
+  Future<List<WebtoonModel>> getTodayWebtoon() async {
     final url = Uri.parse('$baseUrl$todayPath');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      List<WebtoonModel> convertedWebtoons = [];
+
+      for (var webtoon in webtoons) {
+        final webtoonModel = WebtoonModel.fromJson(webtoon);
+        convertedWebtoons.add(webtoonModel);
+      }
+      return convertedWebtoons;
     }
     throw Exception("Today Server ERROR");
   }
